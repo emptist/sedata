1f91588c439e4571bdddfe160fbce1df932a25ab
jk <emptist@users.noreply.github.com>
2016-02-05 11:46:06 +0800

    minor updat

--- lib/getjson.py
+++ lib/getjson.py
@@ -1,18 +1,20 @@
-import sys
-import io
+import sys,json,io
+# import io
 import tushare as ts
-import json
 
 # http://stackoverflow.com/questions/16549332/python-3-how-to-specify-stdin-encoding
 # input_stream = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
 for line in sys.stdin:
     message = json.loads(line)
-    func = message['func']
-    # TODO: handle args
-    args = message['args'][0]
 
-    if func:
-        data = ts.__getattribute__(func)(args).to_json()
+    if message.get('func'):
+        funcname = message['func']
+        if message.get('args'):
+            args = message['args']
+            func = "ts.{funcname}( {args} )".format(funcname=funcname,args=args)
+            data = (eval(func)).to_json()
+        else:
+            data = ts.__getattribute__(funcname).to_json()
         print(data)
     else:
-        print(json.dumps(message))
+        sys.stderr.write(json.dumps(message))
--- src/example.coffee
+++ src/example.coffee
@@ -1,9 +1,9 @@
 {ticks,tsdata} = require 'sedata'
 
+tsdata {func:'get_hist_data', args:"'300388',ktype='W'"}, (res)->
+  console.log res
+
 codes = "900901,000002sz,600603sh,200429,159915"
 ticks codes, (obj)->
   {buy,name,ask5} = obj.sz159915
   console.log('here:',buy,name,ask5)
-
-tsdata {func:'get_hist_data', args:['300388']}, (res)->
-  console.log res
