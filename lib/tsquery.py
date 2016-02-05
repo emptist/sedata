b8e75bf9a167b870f3855ca3867426135387f04a
jk <emptist@users.noreply.github.com>
2016-02-05 22:14:57 +0800

    patch update

---
+++ lib/tsquery.py
@@ -0,0 +1,20 @@
+#-*- coding:UTF-8 -*-
+
+import json
+import tushare as ts
+
+def query(line):
+    message = json.loads(line)
+
+    data = json.dumps(message)
+    if message.get('func'):
+        funcname = message['func']
+        if message.get('args'):
+            args = message['args']
+            func = "ts.{funcname}( {args} )".format(funcname=funcname,args=args)
+            data = (eval(func)).to_json()
+        else:
+            data = ts.__getattribute__(funcname).to_json()
+        return data
+    else:
+        raise Error(data)
