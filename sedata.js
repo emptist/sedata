394b7efac194baa5fcd1d4440aa114db9018f7c9
jk <emptist@users.noreply.github.com>
2016-02-06 19:19:08 +0800

    patch update

--- sedata.js
+++ sedata.js
@@ -3,12 +3,11 @@ sedata - sedata.js
  Copyright(c) 2016 Jigme Ko <jigme1968@gmail.com>
  MIT Licensed
  */
-var sinadata,tsdata,tsocket;
+var sinadata,tsdata;
 sinadata = require('./lib/sinadata');
 tsdata = require('./lib/tsdata');
-tsocket = require('./lib/tsocket');
+
 module.exports = {
   ticks: sinadata.ticks,
-  tsdata: tsdata,
-  tsocket: tsocket
+  tsdata: tsdata
 };
